7fdbaa55f849ee5ff12c446981dca094c232d9ae
Kutsalaba <kutsalaba.nazariy@gmail.com>
2022-07-23 21:21:56 +0300

    Delete unnecessary AppColor

--- lib/widgets/message_widget.dart
+++ lib/widgets/message_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:mercury/models/message.dart';
-import 'package:mercury/resourses/app_color.dart';
 
 import 'triangle_clippers.dart';
 
